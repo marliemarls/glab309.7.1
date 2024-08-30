ec4f732e6038bbdf2d24d69044d8011fd4ef6d24
Marlie Martinez <marlieahue@gmail.com>
2024-08-30 10:02:55 -0400

    finished glab310.1.1

--- src/main/java/com/perscholas/glab30971/Repository/TutorialRepository.java
+++ src/main/java/com/perscholas/glab30971/Repository/TutorialRepository.java
@@ -1,13 +1,14 @@
 package com.perscholas.glab30971.Repository;
 import com.perscholas.glab30971.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
     List<Tutorial> findByPublished(boolean published);
 }
